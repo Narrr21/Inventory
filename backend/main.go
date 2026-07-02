e497e0705a67b62746b927b192d79cd00320da34
Nadhif <nadhifalrozin@gmail.com>
2026-07-02 08:20:52 +0700

    Fix: wrong config vercel and go

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    "my-backend/api" 
+	"log"
+	"net/http"
+	"os"
+
+	"my-backend/api"
 )
 
 func main() {
-    http.HandleFunc("/api/hello", handler.Handler)
-    log.Println("Server lokal berjalan di port 8080...")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/hello", handler.Handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback untuk development lokal
+	}
+
+	log.Printf("Server berjalan di port %s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
